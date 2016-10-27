4fa3787681f4253bb5bf7de035a8ca7f06897105
liueri19 <liueri19@7hills.org>
2016-10-26 22:12:01 -0400

    update

---
+++ src/org/sevenhills/liueri19/Calculator.java
@@ -0,0 +1,154 @@
+package org.sevenhills.liueri19;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Calculator {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String expression;
+		
+		System.out.println("//Calculator Started//");
+		while (true) {
+			System.out.println("input expression:");
+			expression = scanner.nextLine();
+			
+			if (expression.equalsIgnoreCase("exit"))
+				break;
+			
+			System.out.printf("= %f%n", evaluate(expression));
+		}
+		scanner.close();
+	}
+	
+	public static double evaluate(String expression) {
+		int subStart = 0;
+		boolean isSubExpression = false;
+		String subExpression = "";
+		
+		String tempResultString = "";
+		double finalResult = 0;
+		double number;
+		List<Double> numbers = new ArrayList<Double>();
+		List<Operation> operations = new ArrayList<Operation>();
+		
+		for (int i = 0; i < expression.length(); i++) {
+			if (expression.charAt(i) == '(') {
+				isSubExpression = true;
+				subStart = i;
+			}
+			else if (expression.charAt(i) == ')') {
+				subExpression = Double.toString(evaluate(subExpression));
+				//expression = partBefore + evaluate(subExpression) + partAfter;
+				expression = expression.substring(0, subStart) 
+						+ subExpression 
+						+ expression.substring(i+1);
+				//reset states
+				i = subStart + subExpression.length();
+				subStart = 0;
+				isSubExpression = false;
+				subExpression = "";
+			}
+			if (isSubExpression)
+				subExpression += expression.charAt(i);
+		}
+		
+		for (int i = 0; i < expression.length(); i++) {
+			char ch = expression.charAt(i);
+			if (Character.isDigit(ch) || ch == '.')
+				tempResultString += ch;
+			else if (ch == '+') {
+				number = Double.parseDouble(tempResultString);
+				tempResultString = "";
+				numbers.add(number);
+				operations.add(Operation.ADDITION);
+			}
+			else if (ch == '-') {
+				number = Double.parseDouble(tempResultString);
+				tempResultString = "";
+				numbers.add(number);
+				operations.add(Operation.SUBTRACTION);
+			}
+			else if (ch == '*') {
+				number = Double.parseDouble(tempResultString);
+				tempResultString = "";
+				numbers.add(number);
+				operations.add(Operation.MULTIPLICATION);
+			}
+			else if (ch == '/') {
+				number = Double.parseDouble(tempResultString);
+				tempResultString = "";
+				numbers.add(number);
+				operations.add(Operation.DIVISION);
+			}
+			else if (ch == '^') {
+				number = Double.parseDouble(tempResultString);
+				tempResultString = "";
+				numbers.add(number);
+				operations.add(Operation.EXPONENTIATION);
+			}
+		}
+		//after the for loop, add the last number
+		number = Double.parseDouble(tempResultString);
+		tempResultString = "";
+		numbers.add(number);
+		
+		//process exponentiation
+		for (int i = 0; i < operations.size(); i++) {
+			Operation op = operations.get(i);
+			double result;
+			if (op == Operation.EXPONENTIATION) {
+				result = calculate(op, numbers.get(i), numbers.get(i+1));
+				numbers.set(i+1, result);
+				numbers.remove(i);
+				operations.remove(i);
+				i--;
+			}
+		}
+		
+		//process multiplication and division
+		for (int i = 0; i < operations.size(); i++) {
+			Operation op = operations.get(i);
+			double result;
+			if (op == Operation.MULTIPLICATION || op == Operation.DIVISION) {
+				result = calculate(op, numbers.get(i), numbers.get(i+1));
+				numbers.set(i+1, result);
+				numbers.remove(i);
+				operations.remove(i);
+				i--;
+			}
+		}
+		
+		//at last, process addition and subtraction
+		finalResult = numbers.get(0);
+		for (int i = 0; i < operations.size(); i++) {
+			Operation op = operations.get(i);
+			finalResult = calculate(op, finalResult, numbers.get(i+1));
+		}
+		
+		return finalResult;
+	}
+	
+	public static double calculate(Operation op, double a, double b) {
+		if (op == Operation.ADDITION)
+			return a + b;
+		else if (op == Operation.SUBTRACTION)
+			return a - b;
+		else if (op == Operation.MULTIPLICATION)
+			return a * b;
+		else if (op == Operation.DIVISION)
+			return a / b;
+		else if (op == Operation.EXPONENTIATION)
+			return Math.pow(a, b);
+		throw new IllegalArgumentException();
+	}
+}
+
+enum Operation {
+	ADDITION,
+	SUBTRACTION,
+	MULTIPLICATION,
+	DIVISION,
+	EXPONENTIATION;
+}
