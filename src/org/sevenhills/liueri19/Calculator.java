9ec721c8ebe1ccfce34dfb74d94f3a768a744986
liueri19 <liueri19@7hills.org>
2016-11-15 21:06:20 -0500

    Update Calculator.java

--- src/org/sevenhills/liueri19/Calculator.java
+++ src/org/sevenhills/liueri19/Calculator.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Calculator {
-	public static final String VERSION = "1.1";
+	public static final String VERSION = "1.2pre1";
 	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
